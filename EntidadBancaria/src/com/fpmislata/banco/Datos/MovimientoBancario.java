a6c1b11107ca33137640761cde6984367e101063
abel2012 <abelpfon2010@gmail.com>
2013-09-26 10:22:29 +0200

    hola

--- EntidadBancaria/src/com/fpmislata/banco/Datos/MovimientoBancario.java
+++ EntidadBancaria/src/com/fpmislata/banco/Datos/MovimientoBancario.java
@@ -19,4 +19,100 @@ public class MovimientoBancario {
     private String saldoTotal;
     private String concepto;
     
+    public MovimientoBancario(){}
+    
+    public MovimientoBancario(String idMovimientoBancario,TipoMovimientoBancario tipoMovimientoBancario,String importe,Date fecha,String saldoTotal,String concepto){
+    
+     this.idMovimientoBancario=idMovimientoBancario;
+     this.tipoMovimientoBancario=tipoMovimientoBancario;
+     this.importe=importe;
+     this.fecha=fecha;
+     this.saldoTotal=saldoTotal;
+     this.concepto=concepto;
+    
+    }
+
+    /**
+     * @return the idMovimientoBancario
+     */
+    public String getIdMovimientoBancario() {
+        return idMovimientoBancario;
+    }
+
+    /**
+     * @param idMovimientoBancario the idMovimientoBancario to set
+     */
+    public void setIdMovimientoBancario(String idMovimientoBancario) {
+        this.idMovimientoBancario = idMovimientoBancario;
+    }
+
+    /**
+     * @return the tipoMovimientoBancario
+     */
+    public TipoMovimientoBancario getTipoMovimientoBancario() {
+        return tipoMovimientoBancario;
+    }
+
+    /**
+     * @param tipoMovimientoBancario the tipoMovimientoBancario to set
+     */
+    public void setTipoMovimientoBancario(TipoMovimientoBancario tipoMovimientoBancario) {
+        this.tipoMovimientoBancario = tipoMovimientoBancario;
+    }
+
+    /**
+     * @return the importe
+     */
+    public String getImporte() {
+        return importe;
+    }
+
+    /**
+     * @param importe the importe to set
+     */
+    public void setImporte(String importe) {
+        this.importe = importe;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the saldoTotal
+     */
+    public String getSaldoTotal() {
+        return saldoTotal;
+    }
+
+    /**
+     * @param saldoTotal the saldoTotal to set
+     */
+    public void setSaldoTotal(String saldoTotal) {
+        this.saldoTotal = saldoTotal;
+    }
+
+    /**
+     * @return the concepto
+     */
+    public String getConcepto() {
+        return concepto;
+    }
+
+    /**
+     * @param concepto the concepto to set
+     */
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
 }
