668f8eb0d037ebaea18df9649bd563e07a5150ac
abel2012 <abelpfon2010@gmail.com>
2013-09-26 10:10:59 +0200

    hola

---
+++ EntidadBancaria/src/com/fpmislata/banco/Datos/MovimientoBancario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpmislata.banco.Datos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author alumno
+ */
+public class MovimientoBancario {
+    
+    private String idMovimientoBancario;
+    private TipoMovimientoBancario tipoMovimientoBancario;
+    private String importe;
+    private Date fecha;
+    private String saldoTotal;
+    private String concepto;
+    
+}
