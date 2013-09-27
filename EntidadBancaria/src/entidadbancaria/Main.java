80aa2c3dc80d986866bc9734926aae836cf3b046
abel2012 <abelpfon2010@gmail.com>
2013-09-27 10:15:08 +0200

    hola

--- EntidadBancaria/src/entidadbancaria/Main.java
+++ EntidadBancaria/src/entidadbancaria/Main.java
@@ -12,6 +12,8 @@ import com.fpmislata.banco.Datos.TipoEntidadBancaria;
 import com.fpmislata.banco.Datos.TipoMovimientoBancario;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -25,43 +27,46 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       
+
         EntidadBancaria entidadBancaria1 = new EntidadBancaria(1, "12346789", "Asaco", "A464578K", TipoEntidadBancaria.BANCO);
         EntidadBancaria entidadBancaria2 = new EntidadBancaria(2, "34562134", "Alibaba y los 40", "A567578S", TipoEntidadBancaria.CAJADEAHORRO);
-               
+
         SucursalBancaria sucursalBancaria1 = new SucursalBancaria(1, entidadBancaria1, "12345678", "bankia");
         SucursalBancaria sucursalBancaria2 = new SucursalBancaria(2, entidadBancaria2, "13456723", "bancaja");
 
-         List<SucursalBancaria> sucursal = entidadBancaria1.getSucursales();
-        
+        List<SucursalBancaria> sucursal = entidadBancaria1.getSucursales();
+
         sucursal.add(sucursalBancaria1);
         sucursal.add(sucursalBancaria2);
-        
+
         BigDecimal num = new BigDecimal("1200");
-num = num.add(new BigDecimal("3.2"));
+        num = num.add(new BigDecimal("3.2"));
 
-BigDecimal num2 = new BigDecimal("1600");
-num2 = num2.add(new BigDecimal("5.3"));
+        BigDecimal num2 = new BigDecimal("1600");
+        num2 = num2.add(new BigDecimal("5.3"));
 
-        CuentaBancaria cuentaBancaria1 = new CuentaBancaria(1,sucursalBancaria1,"ES7721266563156007653456","14",num,"A464578K");
-       CuentaBancaria cuentaBancaria2 = new CuentaBancaria(2,sucursalBancaria2,"ES7734676563156008753456","16",num2,"A567578S");
-       
-       BigDecimal num3 = new BigDecimal("100");
-num3 = num3.add(new BigDecimal("5.3"));
+        CuentaBancaria cuentaBancaria1 = new CuentaBancaria(1, sucursalBancaria1, "ES7721266563156007653456", "14", num, "A464578K");
+        CuentaBancaria cuentaBancaria2 = new CuentaBancaria(2, sucursalBancaria2, "ES7734676563156008753456", "16", num2, "A567578S");
 
-BigDecimal num4 = new BigDecimal("1700");
-num4 = num4.add(new BigDecimal("8.3"));
+        BigDecimal num3 = new BigDecimal("100");
+        num3 = num3.add(new BigDecimal("5.3"));
 
- BigDecimal num5 = new BigDecimal("150");
-num5 = num5.add(new BigDecimal("8.3"));
+        BigDecimal num4 = new BigDecimal("1700");
+        num4 = num4.add(new BigDecimal("8.3"));
 
-BigDecimal num6 = new BigDecimal("1800");
-num6 = num6.add(new BigDecimal("8.3"));
+        BigDecimal num5 = new BigDecimal("150");
+        num5 = num5.add(new BigDecimal("8.3"));
 
-       MovimientoBancario movimientoBancario = new MovimientoBancario(1,TipoMovimientoBancario.DEBE,num3,"23-06-2013",num4,"debe");
-        MovimientoBancario movimientoBancario = new MovimientoBancario(2,TipoMovimientoBancario.HABER,num5,"23-09-1993",num6,"debe");
-       
-    
+        BigDecimal num6 = new BigDecimal("1800");
+        num6 = num6.add(new BigDecimal("8.3"));
+
+         GregorianCalendar gregorianCalendar =new GregorianCalendar(1993,9,23);
+          GregorianCalendar gregorianCalendar2 =new GregorianCalendar(2013,5,17);
+        
+        
+        MovimientoBancario movimientoBancario = new MovimientoBancario(1, TipoMovimientoBancario.DEBE, num3, gregorianCalendar, num4, "debe");
+        MovimientoBancario movimientoBancario = new MovimientoBancario(2, TipoMovimientoBancario.HABER, num5, gregorianCalendar2, num6, "debe");
+
+
     }
-
 }
