eb850b1335e00f86f56de56b02fb2d538f844fa7
abel2012 <abelpfon2010@gmail.com>
2013-10-03 10:43:38 +0200

    hola

--- EntidadBancaria/src/com/fpmislata/banco/Datos/EntidadBancariaDAO.java
+++ EntidadBancaria/src/com/fpmislata/banco/Datos/EntidadBancariaDAO.java
@@ -15,38 +15,72 @@ import java.sql.SQLException;
  * @author alumno
  */
 public class EntidadBancariaDAO {
-    
-    EntidadBancaria read(int idEntidad) throws ClassNotFoundException, SQLException{
-         Class.forName("com.mysql.jdbc.Driver");
-Connection connection = null;
-connection = DriverManager.getConnection(
-	"jdbc:mysql://localhost:3306/banco","banco","banco");
-        
-        int idEntidadBancaria=1;
+ Connection connection = null;
+    public EntidadBancariaDAO() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+       
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/banco", "banco", "banco");
+    }
+
+    EntidadBancaria read(int idEntidad) throws ClassNotFoundException, SQLException {
+
+
+        int idEntidadBancaria = 1;
         String selectSQL = "SELECT * FROM entidadbancaria WHERE idEntidad = ?";
-PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
-preparedStatement.setInt(1, idEntidadBancaria);
-ResultSet rs = preparedStatement.executeQuery();
-while (rs.next()) {
-	String userid = rs.getString("USER_ID");
-	String username = rs.getString("USERNAME");
-        
-        idEntidad  = rs.getInt("idEntidad");
-	String codigoEntidad= rs.getString("codigoEntidad");
-        String nombre = rs.getString("nombre");	
-        String cif = rs.getString("cif");
-        String tipoEntidadBancaria = rs.getString("tipoEntidadBancaria");
-        
-         System.out.println(idEntidad);
-        System.out.println(codigoEntidad);
-        System.out.println(nombre);
-        System.out.println(cif);
-        System.out.println(tipoEntidadBancaria);
-        
-}
+        PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
+        preparedStatement.setInt(1, idEntidadBancaria);
+        ResultSet rs = preparedStatement.executeQuery();
+        while (rs.next()) {
+            String userid = rs.getString("USER_ID");
+            String username = rs.getString("USERNAME");
+
+            idEntidad = rs.getInt("idEntidad");
+            String codigoEntidad = rs.getString("codigoEntidad");
+            String nombre = rs.getString("nombre");
+            String cif = rs.getString("cif");
+            String tipoEntidadBancaria = rs.getString("tipoEntidadBancaria");
+
+            System.out.println(idEntidad);
+            System.out.println(codigoEntidad);
+            System.out.println(nombre);
+            System.out.println(cif);
+            System.out.println(tipoEntidadBancaria);
+
+        }
+        String codigo;
+        if (rs.next() == true) {
+
+            codigo = rs.getString("codigo");
+            if (rs.next() == true) {
+                throw new RuntimeException("Error con la entidad bancaria" + idEntidadBancaria);
+            }
+
+
+        } else {
+            RuntimeException runtimeException = new RuntimeException("no va la excepcion" + idEntidadBancaria);
+            throw runtimeException;
+        }
+
+        rs.next();
         return null;
-}
-    
-    
-    
+    }
+
+    public void insert(EntidadBancaria entidadBancaria) throws SQLException {
+
+        String insertTableSQL = "INSERT INTO entidadbancaria"
+                + "(idEntidad, codigoEntidad, nombre, cif,tipoEntidadBancaria) VALUES"
+                + "(?,?,?,?,?)";
+        PreparedStatement preparedStatement2 = connection.prepareStatement(insertTableSQL);
+        preparedStatement2.setInt(4, 4);
+        preparedStatement2.setString(5, "13672470");
+        preparedStatement2.setString(6, "Ruralcaja");
+        preparedStatement2.setString(7, "14562345");
+        preparedStatement2.setString(8, "Banco");
+// execute insert SQL stetement
+        preparedStatement2.executeUpdate();
+
+    }
+
+    connection.close();
 }
